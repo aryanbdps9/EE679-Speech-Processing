fd3fcddf7e0342b77f4ca1e74cdeef8de01877e8
sabSAThai <pranavsankhe40@gmail.com>
2018-10-06 09:55:10 +0530

    q1 done

--- assignment_2/q1/hparams.py
+++ assignment_2/q1/hparams.py
@@ -1,9 +1,9 @@
 formant_freq = [730, 1090, 2440]
 formant_bw = [100, 100, 100]
 samp_freq = 8000.0
-sig_freq = 120
-time_length = 0.1
-win_size = 20
+sig_freq = 300
+time_length = 0.5
+win_size = 30.0
 dft_len = 1024
 filter_order= [2, 4, 6, 8, 10]
 # F0=120Hz,F1=300Hz,B1=100Hz
--- assignment_2/q1/q1.py
+++ assignment_2/q1/q1.py
@@ -9,105 +9,76 @@ import sys
 from math import pi
 
 
-def gen_vowel():
-	F1 = np.array(hparams.formant_freq)
-	B1 = np.array(hparams.formant_bw)
-	Fs = hparams.samp_freq
-	T = 1.0/Fs
-	t0 = hparams.time_length
-	num_samples = Fs*t0
-	F0 = hparams.sig_freq
-	t = np.linspace(0, t0, num_samples)
-
-	# sig = signal.sawtooth(2 * np.pi * F0 * t, width=1)
-	sig = signal.square(2 * np.pi * F0 * t, duty=0.01)
-
-	# Calculate pole angles and radii
-	R = np.exp(-pi*B1/Fs)
-	theta = 2*pi*F1/Fs
-
-
-	poles = np.concatenate([R * np.exp(1j*theta), R * np.exp(-1j*theta)])
-	zeros = np.zeros(poles.shape, poles.dtype)
-	b, a = signal.zpk2tf(zeros, poles, 1)
-
-	y = np.zeros(sig.shape, sig.dtype)
-	# time = np.linspace(0, num_samples/Fs, num_samples, endpoint=False)
-
-	for i in range(len(sig)):
-	    y[i] = y[i] + a[0]*sig[i]
-	    for j in range(1, len(a)):
-	        if i-j >= 0:
-	            y[i] = y[i] - a[j]*y[i-j]
-
-	write('120.wav', int(Fs), y)
-
-
 def autocorr(x):
     result = np.correlate(x, x, mode='full')
-    return result[result.size/2:]	
-
-y, sr = librosa.load('120.wav',sr=None) 
-filter_order = hparams.filter_order
-
-for order in filter_order:
-
-	
-	size = order + 1
-	coeffs = np.zeros(size)
-	dummy_coeffs = np.zeros(size)
-	
-
-	r = autocorr(y)
-	sig_eng = r[0]
-	
-	 
-
-	for i in range(1, order + 1): 
-		
-		reflec_coeff = 0
-		dummy_coeffs[1:len(coeffs)] = coeffs[1:len(coeffs)]
-		
-		for j in range(1, i): 
-			reflec_coeff = reflec_coeff + dummy_coeffs[j]*r[i-j]
-
-		coeffs[m] = reflec_coeff	
-		for j in range(1, i):
-			coeffs[j] = dummy_coeffs[j] - reflec_coeff*dummy_coeffs[i - j]  
+    return result[int(result.size/2):]
 
 
-		sig_eng = (1 - np.square(reflec_coeff))	
+sig_freq = hparams.sig_freq
 
+y, samp_freq = librosa.load('300.wav',sr=None)
+win_size = hparams.win_size/1000.0 
+num_samples = int(samp_freq*win_size)
+window = y[:num_samples]*np.hamming(num_samples)
 
-	coeffs[0] = 1.0 
-	coeffs[1:size] = -coeffs[1:size] 	
-	zeros = np.zeros(size)
-	zeros[0] = 1
-	w, h = signal.freqz(zeros, coeffs)
-	plt.plot(hparams.samp_freq*w/(2*pi), 10*order + 20 * np.log10(abs(h)), {2: 'r', 4: 'g', 6: 'b', 8: 'y', 10: 'm'})
+fig1 = plt.figure()
+plt.title('LPC Filters: Frequency Response for /a/ at '+str(sig_freq)+' Hz')
+plt.ylabel('Amplitude [dB]')
+plt.xlabel('Frequency [rad/sample]')
+
+colors = {2: 'r', 4: 'g', 6: 'b', 8: 'y', 10: 'm'}
+orders = hparams.filter_order
+
+for order in orders:
+    
+    R = autocorr(window)
+    error = np.zeros(order+1)
+    error[0] = R[0]
+    G = np.zeros(order + 1)
+
+    coeffs = np.zeros(order+1)
+    dummy_coeffs = np.zeros(order + 1)
+    
+
+    for i in range(1, order +1):
+        reflec_coeffs = 0
+        dummy_coeffs[1:len(coeffs)] = coeffs[1:len(coeffs)]
+        
+        for j in range(1, i):
+            reflec_coeffs = reflec_coeffs + dummy_coeffs[j]*R[i-j]
+        reflec_coeffs = (R[i] - reflec_coeffs)/error[i-1]
+
+        coeffs[i] = reflec_coeffs
+
+        for j in range(1, i):
+            coeffs[j] = dummy_coeffs[j] - reflec_coeffs*dummy_coeffs[i-j]
+
+        error[i] = (1-np.square(reflec_coeffs))*error[i-1]
+
+    coeffs[0] = 1.0
+    coeffs[1:len(coeffs)] = -coeffs[1:len(coeffs)]
+    num_coeffs = np.zeros(coeffs.shape)
+    num_coeffs[0] = 1
+    G[i] = np.sqrt(error[i])
+    w, h = signal.freqz(num_coeffs, coeffs)
+
+    plt.plot(samp_freq*w/(2*np.pi), 10*order + 20 * np.log10(abs(h)), colors[order])
 
 formant_freq = hparams.formant_freq
 formant_bw = hparams.formant_bw
 
-r = np.exp(-pi*formant_bw/hparams.samp_freq)
-theta = 2*pi*formant_freq/hparams.samp_freq
+r = np.exp(np.multiply(-np.pi, formant_bw)/samp_freq)
+theta = 2*np.multiply(np.pi, formant_freq)/samp_freq
 
-poles = np.concatenate([R * np.exp(1j*theta), R * np.exp(-1j*theta)])
+poles = np.concatenate([r * np.exp(1j*theta), r * np.exp(-1j*theta)])
 zeros = np.zeros(poles.shape, poles.dtype)
 
 b, a = signal.zpk2tf(zeros, poles, 1)
 
 wf, hf = signal.freqz(b, a)
 
-plt.plot(hparams.samp_freq*wf/(2*pi), 120 + 20 * np.log10(abs(hf)), 'c')
+plt.plot(samp_freq*wf/(2*pi), 120 + 20 * np.log10(abs(hf)), 'c')
 plt.legend(['Order 2', 'Order 4', 'Order 6', 'Order 8', 'Order 10', 'Orig'])
 plt.grid()
 
-
 plt.show()
-
-
-
-
-
-
